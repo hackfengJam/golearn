8f932c7a145abc1ea73e74a1e294a0d933676666
HackFun <hackfeng624@gmail.com>
2019-04-23 23:59:10 +0800

    接口值类型

--- queue/queue.go
+++ queue/queue.go
@@ -1,15 +1,49 @@
 package queue
 
+// 1
+//type Queue []interface{}
+//func (q *Queue) Push(v interface{}) {
+//	*q = append(*q, v)
+//}
+//
+//func (q *Queue) Pop() interface{} {
+//	head := (*q)[0]
+//	*q = (*q)[1:]
+//	return head
+//}
+//
+//func (q *Queue) IsEmpty() bool {
+//	return len(*q) == 0
+//}
+
+// 2
+//type Queue []interface{}
+//
+//func (q *Queue) Push(v int) {
+//	*q = append(*q, v)
+//}
+//
+//func (q *Queue) Pop() int {
+//	head := (*q)[0]
+//	*q = (*q)[1:]
+//	return head.(int)
+//}
+//
+//func (q *Queue) IsEmpty() bool {
+//	return len(*q) == 0
+//}
+
+// 3
 type Queue []interface{}
 
 func (q *Queue) Push(v interface{}) {
-	*q = append(*q, v)
+	*q = append(*q, v.(int))
 }
 
 func (q *Queue) Pop() interface{} {
 	head := (*q)[0]
 	*q = (*q)[1:]
-	return head
+	return head.(int)
 }
 
 func (q *Queue) IsEmpty() bool {
