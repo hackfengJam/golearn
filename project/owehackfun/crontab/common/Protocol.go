13d164564d626f832452fab5d0b12d9a29b8b2c3
HackFun <hackfeng624@gmail.com>
2019-06-02 22:36:21 +0800

    8-1~3

--- project/owehackfun/crontab/common/Protocol.go
+++ project/owehackfun/crontab/common/Protocol.go
@@ -1,6 +1,9 @@
 package common
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"strings"
+)
 
 // 定时任务
 type Job struct {
@@ -14,6 +17,10 @@ type Response struct {
 	Errno int         `json:"errno"`
 	Msg   string      `json:"msg"`
 	Data  interface{} `json:"data"`
+}
+type JobEvent struct {
+	EventType int // SAVE, DELETE
+	Job       *Job
 }
 
 // 应答方法
@@ -31,4 +38,32 @@ func BuildResponse(errno int, msg string, data interface{}) (resp []byte, err er
 	resp, err = json.Marshal(response)
 
 	return
+}
+
+// 反序列化 Job
+func UnpackJob(value []byte) (ret *Job, err error) {
+	var (
+		job *Job
+	)
+	job = &Job{}
+
+	if err = json.Unmarshal(value, job); err != nil {
+		return
+	}
+	ret = job
+	return
+}
+
+// 从etcd 的 key 提取任务名
+// /cron/jobs/job10 抹掉 /cron/jobs
+func ExtractJobName(jobKey string) (string) {
+	return strings.TrimPrefix(jobKey, JOB_SAVE_DIR)
+}
+
+// 任务变化事件有两种 1. 更新任务 2. 删除任务
+func BuildJobEvent(eventType int, job *Job) (jobEvent *JobEvent) {
+	return &JobEvent{
+		EventType: eventType,
+		Job:       job,
+	}
 }
