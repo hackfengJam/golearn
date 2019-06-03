6184d5cd665f74b1d637b789143f7eb9da278bf4
HackFun <hackfeng624@gmail.com>
2019-06-03 22:26:45 +0800

    利用分布式锁避免任务并发

--- project/owehackfun/crontab/worker/Executor.go
+++ project/owehackfun/crontab/worker/Executor.go
@@ -20,10 +20,11 @@ var (
 func (executor *Executor) ExecuteJob(info *common.JobExecuteInfo) {
 	go func() {
 		var (
-			cmd    *exec.Cmd
-			err    error
-			output []byte
-			result *common.JobExecuteResult
+			cmd     *exec.Cmd
+			err     error
+			output  []byte
+			result  *common.JobExecuteResult
+			jobLock *JobLock
 		)
 
 		// 任务结果
@@ -32,19 +33,36 @@ func (executor *Executor) ExecuteJob(info *common.JobExecuteInfo) {
 			Output:      make([]byte, 0),
 		}
 
+		// 首先获取分布式锁
+		jobLock = G_jobMgr.CreateJobLock(info.Job.Name)
+
 		// 记录任务开始时间
 		result.StartTime = time.Now()
 
-		// 执行 shell 命令
-		cmd = exec.CommandContext(context.TODO(), "E:\\ProgramFiles\\Git\\bin\\bash.exe", "-c", info.Job.Command)
+		// 上锁
+		err = jobLock.TryLock()
+		// 释放锁，注：由于 jobLock.isLocked 的存在，不用担心是否上锁成功
+		defer jobLock.Unlock()
 
-		// 执行并捕获输出
-		output, err = cmd.CombinedOutput()
+		if err != nil { // 上锁失败
+			result.Err = err
+			result.EndTime = time.Now()
+		} else {
+			// 上锁成功后，重置任务启动时间
+			result.StartTime = time.Now()
 
-		// 记录任务结束时间
-		result.EndTime = time.Now()
-		result.Output = output
-		result.Err = err
+			// 执行 shell 命令
+			cmd = exec.CommandContext(context.TODO(), "E:\\ProgramFiles\\Git\\bin\\bash.exe", "-c", info.Job.Command)
+
+			// 执行并捕获输出
+			output, err = cmd.CombinedOutput()
+
+			// 记录任务结束时间
+			result.EndTime = time.Now()
+			result.Output = output
+			result.Err = err
+
+		}
 
 		// 任务执行完成后，把执行的结果返回给 Scheduler， Scheduler会从 executingTable 删除掉执行记录
 		G_scheduler.PushJobResult(result)
